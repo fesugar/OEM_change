047d982acbc0308818db23e838ac7d4835593105
fesugar <fesugar@fesugar.com>
2019-10-14 18:01:02 +0800

    重构资源目录，减少重复项目
    清理了一些不必要的文件

---
+++ Project_OEM_change_CSharp/package.cmd
@@ -0,0 +1,33 @@
+:: 整理资源和程序打包最终输出
+@echo off
+set pt=%~dp0
+del exe\ /s /q
+rd exe\ /s /q
+
+xcopy bin\Release\OEMchange.exe exe\ /i /f /v /h /y
+xcopy ..\Resources\bmp exe\bmp\ /i /f /v /h /y
+xcopy ..\Resources\content.json exe\ /i /f /v /h /y
+xcopy ..\Resources\content.xml exe\ /i /f /v /h /y
+xcopy bin\Release\readme.txt exe\ /i /f /v /h /y
+xcopy ..\Resources\string.ini exe\ /i /f /v /h /y
+
+xcopy ..\packages\MetroModernUI.1.4.0.0\lib\net\MetroFramework.Design.dll exe\ /i /f /v /h /y
+xcopy ..\packages\MetroModernUI.1.4.0.0\lib\net\MetroFramework.dll exe\ /i /f /v /h /y
+xcopy ..\packages\MetroModernUI.1.4.0.0\lib\net\MetroFramework.Fonts.dll exe\ /i /f /v /h /y
+
+xcopy ..\packages\Newtonsoft.Json.12.0.2\lib\net40\Newtonsoft.Json.dll exe\ /i /f /v /h /y
+
+::合并DLL  EXE
+..\tools\ILMerge.exe  /ndebug /keyfile:..\OEM_change.snk /target:winexe /targetplatform:v4 /out:%pt%\exe\OEM_change.exe /log %pt%\exe\OEMchange.exe %pt%\exe\MetroFramework.Design.dll %pt%\exe\MetroFramework.dll %pt%\exe\MetroFramework.Fonts.dll %pt%\exe\Newtonsoft.Json.dll
+
+del exe\OEMchange.exe
+del exe\MetroFramework.Design.dll
+del exe\MetroFramework.dll
+del exe\MetroFramework.Fonts.dll
+del exe\Newtonsoft.Json.dll
+
+
+if exist %pt%\exe\OEM_change.exe (rename %pt%\exe\OEM_change.exe OEMchange.exe && echo success) else (echo ERROR) 
+
+
+pause
---
+++ README.md
@@ -0,0 +1,15 @@
+# OEM_change
+修改OEM注册表信息
+# 软件介绍：
+  修改电脑Windows系统OEM显示信息
+  
+# 新增功能：
+  1. 添加10个品牌机预设方案；
+  2. 可以自行配置预置的OEM方案；
+  
+# 支持操作系统： 
+  Windows 7 SP1
+  
+  Windows 8.1
+  
+  Windows 10
